2ab402f07837974efcf5f5005338e6c4c2a38deb
Alex Ling <hkalexling@gmail.com>
2024-11-09 22:34:35 +0700

    c + emcc

--- app.js
+++ app.js
@@ -1,41 +1,59 @@
+import module from "./libkeisan.mjs";
+
+import datetime from "./plugins/datetime.mjs";
+import unit from "./plugins/unit.mjs";
+import percent from "./plugins/percent.mjs";
+
 const { h, render, Component } = preact;
 const html = htm.bind(h);
 
-const readNullTerminatedString = (wasm, ptr) => {
-  const memoryView = new Uint8Array(wasm.memory.buffer);
-  let str = '';
-  for (let i = ptr; memoryView[i] !== 0; i++) {
-    str += String.fromCharCode(memoryView[i]);
+class Store {
+  constructor() {
+    this.store = {};
+    this.storeId = 0;
   }
-  return str;
-};
 
-const allocateString = (wasm, str) => {
-  const { memory, wasm_alloc } = wasm;
-  const strBuf = new TextEncoder().encode(str);
-  const ptr = wasm_alloc(strBuf.length + 1);
-  const memoryView = new Uint8Array(memory.buffer);
-  for (let i = 0; i < strBuf.length; i++) {
-    memoryView[ptr + i] = strBuf[i];
+  set(value) {
+    if (value === undefined) return;
+    this.store[this.storeId] = value;
+    return this.storeId++;
   }
-  memoryView[ptr + strBuf.length] = 0;
-  return ptr;
-};
 
-const allocateOptions = (wasm, options) => {
-  const { memory, wasm_alloc } = wasm;
-  const ptr = wasm_alloc(8);
-  const memoryView = new DataView(memory.buffer);
-  memoryView.setUint32(ptr, options.max_size, true);
-  memoryView.setUint32(ptr + 4, options.verbose, true);
-  return ptr;
+  get(id) {
+    return this.store[id];
+  }
+
+  del(id) {
+    delete this.store[id];
+  }
+
+  dup(id) {
+    const value = this.store[id];
+    return this.set(value);
+  }
+
+  print() {
+    console.log(this.store);
+  }
 }
+
+globalThis.plugins = {
+  datetime,
+  unit,
+  percent,
+};
+
+globalThis.store = new Store();
+
+globalThis.keisan = {
+  log: console.log,
+};
 
 class App extends Component {
   constructor() {
     super();
-    this.state = { 
-      input: '',
+    this.state = {
+      input: "",
       results: [],
       loading: true,
       wasm: null,
@@ -45,99 +63,65 @@ class App extends Component {
     this.throttleTimeout = null;
   }
 
-  getRates = async () => {
-    try {
-      const stored = JSON.parse(localStorage.getItem('rates.json'));
-      if (stored.time_next_update_unix > Date.now() / 1000)
-        return stored;
-    } catch (e) {}
-    console.log('Fetching rates...');
-    const res = await fetch('https://open.er-api.com/v6/latest/USD');
-    const rates = await res.json();
-    localStorage.setItem('rates.json', JSON.stringify(rates));
-  };
-
   initWasm = async () => {
-    const res = await fetch('./libkeisan.wasm');
-    const bytes = await res.arrayBuffer();
-    const module = await WebAssembly.instantiate(bytes, {
-      env: {
-        get_time: () => BigInt(Date.now()) * 1000n,
-        get_gmt_offset: () => new Date().getTimezoneOffset() * -60,
-        log: (ptr) => {
-          const str = readNullTerminatedString(this.state.wasm, ptr);
-          console.log(str);
-        },
-        dpow: Math.pow,
-        save: (filenamePtr, contentPtr) => {
-          const filename = readNullTerminatedString(this.state.wasm, filenamePtr);
-          const content = readNullTerminatedString(this.state.wasm, contentPtr);
-          localStorage.setItem(filename, content);
-          const byteSize = new TextEncoder().encode(content).length;
-          return byteSize;
-        },
-        load: (filenamePtr, contentPtr) => {
-          const filename = readNullTerminatedString(this.state.wasm, filenamePtr);
-          const content = localStorage.getItem(filename);
-          if (content === undefined) {
-            return 0;
-          }
-          const ptr = allocateString(this.state.wasm, content);
-          return ptr;
-        },
-      }
-    });
-
-    const wasm = module.instance.exports;
-    wasm._initialize();
-
-    const options = {
-      max_size: 1,
-      verbose: 1,
-    };
-    const optPtr = allocateOptions(wasm, options);
-    const ctx = wasm.ks_init(optPtr);
-
+    const wasm = await module();
+    const ctx = wasm._api_new();
     this.setState({ wasm, ctx });
   };
 
   componentDidMount = async () => {
     try {
-      await Promise.all([this.getRates(), this.initWasm()]);
+      await this.initWasm();
       this.setState({ loading: false });
     } catch (error) {
       console.error("Error initializing:", error);
     }
-  }
+  };
 
   componentWillUnmount = () => {
     if (this.throttleTimeout) clearTimeout(this.throttleTimeout);
-    if (this.state.wasm && this.state.ctx) {
-      this.state.wasm.ks_free(this.state.ctx);
+    if (this.state.wasm) {
+      this.state.wasm._api_free(this.state.ctx);
     }
-  }
+  };
+
+  parseContext = () => {
+    const { wasm, ctx } = this.state;
+
+    const count = wasm.getValue(ctx, "i32");
+    const valuesPtr = wasm.getValue(ctx + 4, "i32");
+    const errorsPtr = wasm.getValue(ctx + 8, "i32");
+
+    const values = [];
+    for (let i = 0; i < count; i++) {
+      const valuePtr = wasm.getValue(valuesPtr + i * 4, "i32");
+      const value = wasm.UTF8ToString(valuePtr);
+      values.push(value);
+    }
+
+    const errors = [];
+    for (let i = 0; i < count; i++) {
+      const error = wasm.getValue(errorsPtr + i, "i8");
+      errors.push(Boolean(error));
+    }
+
+    return { count, values, errors };
+  };
 
   processLine = (line) => {
-    if (!line.trim()) return { type: 'result', value: '' };
+    if (!line.trim()) return { type: "result", value: "" };
 
     const { wasm, ctx } = this.state;
 
-    const inputPtr = allocateString(wasm, line);
-    const res = wasm.ks_evaluate(ctx, inputPtr);
-    wasm.wasm_free(inputPtr);
+    const cstr = wasm.stringToNewUTF8(line);
+    wasm._api_evaluate(ctx, cstr);
+    wasm._free(cstr);
 
-    const memory = new DataView(wasm.memory.buffer);
-
-    if (res) {
-      const errPtr = memory.getUint32(ctx + 4, true);
-      return { type: 'error', value: readNullTerminatedString(wasm, errPtr) };
-    }
-
-    const valuesPtr = memory.getUint32(ctx, true);
-    const resultPtr = memory.getUint32(valuesPtr, true);
-    const resultValue = readNullTerminatedString(wasm, resultPtr);
-    return { type: 'result', value: resultValue };
-  }
+    const { count, values, errors } = this.parseContext(ctx);
+    if (count === 0) return { type: "result", value: "" };
+    if (errors[0]) return { type: "error", value: values[0] };
+    return { type: "result", value: values[0] };
+  };
 
   handleInput = (event) => {
     const input = event.target.value;
@@ -148,7 +132,7 @@ class App extends Component {
     }
 
     this.throttleTimeout = setTimeout(() => {
-      const lines = input.split('\n');
+      const lines = input.split("\n");
       const results = lines.map(this.processLine);
       this.setState({ results });
     }, 100);
@@ -158,8 +142,8 @@ class App extends Component {
     if (this.state.loading) {
       return html`
         <div class="loading">
-            <span>Loading...</span>
-            <div class="spinner"></div>
+          <span>Loading...</span>
+          <div class="spinner"></div>
         </div>
       `;
     }
@@ -171,20 +155,20 @@ class App extends Component {
           onInput=${this.handleInput}
           value=${this.state.input}
           ref=${this.inputRef}
-          spellCheck="false"
+          spellcheck="false"
           autocorrect="off"
           autocapitalize="off"
         ></textarea>
         <div id="results-area">
-          ${this.state.results.map(result => html`
-            <div class=${result.type}>
-              ${result.value || ' '}
-            </div>
-          `)}
+          ${this.state.results.map(
+            (result) => html`
+              <div class=${result.type}>${result.value || " "}</div>
+            `,
+          )}
         </div>
       </div>
     `;
   }
 }
 
-render(html`<${App} />`, document.getElementById('app'));
+render(html`<${App} />`, document.getElementById("app"));
